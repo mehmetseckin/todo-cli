be1a3af6ae993e81015ef60c8c3f915e26733825
Mehmet Seckin <1876867+mehmetseckin@users.noreply.github.com>
2019-10-09 23:30:13 +0000

    Add a simple CLI application
    Add a starter CLI application that prints out
    its name, version and description.

---
+++ src/Microsoft.Todo.CLI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Reflection;
+
+namespace Microsoft.Todo.CLI
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // Define the root command
+            var rootCommand = new RootCommand
+            {
+                new Option(new string[] { "-v", "--version" } , "Prints out the todo CLI version.")
+                {
+                    Argument = new Argument<bool>()
+                }
+            };
+
+            rootCommand.Description = "A CLI to manage to do items.";
+
+            // Define handler
+            rootCommand.Handler = CommandHandler.Create<bool>((version) =>
+            {
+                if (version)
+                {
+                    PrintVersion();
+                    return;
+                }
+            });
+
+            return rootCommand.InvokeAsync(args).Result;
+        }
+
+        private static void PrintVersion()
+        {
+            var entryAssembly = Assembly.GetEntryAssembly();
+            var entryAssemblyName = entryAssembly.GetName();
+            var description = entryAssembly.GetCustomAttribute<AssemblyDescriptionAttribute>()?.Description;
+            Console.WriteLine($"{entryAssemblyName.Name} {entryAssemblyName.Version} - {description}");
+        }
+    }
+}
