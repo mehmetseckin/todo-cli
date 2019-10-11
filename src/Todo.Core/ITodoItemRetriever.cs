a5afc14fc8dfdde24d106408a2ed57b382394a60
Mehmet Seckin <1876867+mehmetseckin@users.noreply.github.com>
2019-10-11 00:03:27 +0000

    Add a core library and a class to retrieve todo items

---
+++ src/Todo.Core/ITodoItemRetriever.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Core
+{
+    public interface ITodoItemRetriever
+    {
+        Task<IEnumerable<OutlookTask>> ListAsync();
+    }
+}
