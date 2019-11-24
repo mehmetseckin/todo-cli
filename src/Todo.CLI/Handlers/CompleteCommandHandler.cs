fd1983e39b944e799f99de4bccea05da6d7a8335
Mehmet Seckin <mehmet@seckin.me>
2019-11-24 23:29:35 +0000

    Add multiple selection support for complete command

--- src/Todo.CLI/Handlers/CompleteCommandHandler.cs
+++ src/Todo.CLI/Handlers/CompleteCommandHandler.cs
@@ -1,8 +1,10 @@
 ﻿using InquirerCS;
 using System;
+using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Todo.Core;
@@ -12,33 +14,45 @@ namespace Todo.CLI.Handlers
 {
     public class CompleteCommandHandler
     {
+        private const string PromptMessage = "Which item(s) would you like to delete?";
+        private const string UIHelpMessage = "Use arrow keys to navigate between options. [SPACEBAR] to mark the options, and [ENTER] to confirm your input.";
+
         public static ICommandHandler Create(IServiceProvider serviceProvider)
         {
             return CommandHandler.Create<string>(async (itemId) =>
             {
                 var todoItemRepository = (ITodoItemRepository)serviceProvider.GetService(typeof(ITodoItemRepository));
+                
                 if (!string.IsNullOrEmpty(itemId))
                 {
-                    await CompleteItem(todoItemRepository, new TodoItem { Id = itemId });
+                    var item = new TodoItem { Id = itemId };
+                    await todoItemRepository.CompleteAsync(item);
                 }
                 else
                 {
                     // Retrieve items that are not completed
                     var items = await todoItemRepository.ListAsync(listAll: false);
 
-                    // Ask user which item to complete
-                    var selectedItem = Question
-                        .List("Which item would you like to complete?", items)
+                    // Ask user which items to complete
+                    var message = PromptMessage
+                                + Environment.NewLine
+                                + Environment.NewLine
+                                + UIHelpMessage;
+                    
+                    var selectedItems = Question
+                        .Checkbox(message, items)
                         .Prompt();
-                    await CompleteItem(todoItemRepository, selectedItem);
+
+                    CompleteItems(todoItemRepository, selectedItems);
                 }
+                
+                Console.Clear();
             });
         }
 
-        private static async Task CompleteItem(ITodoItemRepository todoItemRepository, TodoItem selectedItem)
+        private static void CompleteItems(ITodoItemRepository todoItemRepository, IEnumerable<TodoItem> selectedItems)
         {
-            await todoItemRepository.CompleteAsync(selectedItem);
-            Console.Clear();
+            Task.WaitAll(selectedItems.Select(item => todoItemRepository.CompleteAsync(item)).ToArray());
         }
     }
 }
